8857f29a191f1b92f1aead303d940afad801aa1a
u1095279207515 <1095279207515@tianchi.com>
2019-02-21 00:20:20 +0800

    增加特征,修改参数

--- scoring_credits.py
+++ scoring_credits.py
@@ -3,7 +3,8 @@ import os
 import sys
 from sklearn.model_selection import train_test_split
 import lightgbm as lgb
-
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 TRAIN_DATA = "./data/train_dataset.csv"
 TEST_DATA = "./data/test_dataset.csv"
 
@@ -16,7 +17,7 @@ def metrics_mae(label, score):
 if __name__ == "__main__":
     train = pd.read_csv(TRAIN_DATA, encoding="utf-8")
     test = pd.read_csv(TEST_DATA, encoding="utf-8")
-    print(train.columns)
+    #print(train.columns)
     '''
     Index(['用户编码', '用户实名制是否通过核实', '用户年龄', '是否大学生客户', '是否黑名单客户', '是否4G不健康客户',
        '用户网龄（月）', '用户最近一次缴费距今时长（月）', '缴费用户最近一次缴费金额（元）', '用户近6个月平均消费值（元）',
@@ -29,52 +30,106 @@ if __name__ == "__main__":
     '''
     #print(train.describe())
     #print(train[['用户实名制是否通过核实']].groupby("用户实名制是否通过核实").size())
-    print(train[['当月飞机类应用使用次数','信用分']].groupby("当月飞机类应用使用次数").mean())
+    #print(train[['当月通话交往圈人数','信用分']].groupby("当月通话交往圈人数").mean())
     #train.apply(lambda row: int(round(row.信用分)), axis=1)
-    #sys.exit(-1)
-    #train['is_plane_user'] = train.apply(lambda row: 1 if row.当月飞机类应用使用次数>row.当月火车类应用使用次数 else 0, axis=1)
-    #train['out_counts'] = train.apply(lambda row: row.当月飞机类应用使用次数 + row.当月火车类应用使用次数, axis=1)
-    #train['shop_counts'] = train.apply(lambda row: row.当月网购类应用使用次数 + row.当月物流快递类应用使用次数, axis=1)
-    #train['用户当月余额总数'] = train.apply(lambda row: row['用户账单当月总费用（元）'] + row['用户当月账户余额（元）'], axis=1)
-    #train['用户开网年龄'] = train.apply(lambda row: row['用户年龄'] - row['用户网龄（月）']/12.0, axis=1)
+    # print(train.isnull().any())
+    # from matplotlib import pyplot as plt
+    # fig, ax = plt.subplots()
+    # ax.scatter(x=train['当月通话交往圈人数'], y=train['信用分'])
+    # plt.ylabel('当月通话交往圈人数', fontsize=5)
+    # plt.xlabel('信用分', fontsize=5)
+    # plt.show()
 
+    # col_list = list(train.columns)
+    # for col in col_list:
+    #     print(col)
+    #     if (col=="用户编码"):
+    #         continue
+    #     print(train[col].skew())
+    # sys.exit(-1)
+
+
+    train['用户当月消费是否低于近6个月'] = train.apply(lambda row: 1 if row['用户账单当月总费用（元）'] > row['用户近6个月平均消费值（元）'] else 0, axis=1)
+    train['用户余额能支持月数'] = train.apply(lambda row: row['用户当月账户余额（元）'] / (row['用户近6个月平均消费值（元）']+1.0) , axis=1)
+    train['用户最近缴费支持月数'] = train.apply(lambda row: (row['缴费用户最近一次缴费金额（元）']) / (row['用户近6个月平均消费值（元）']+1.0), axis=1)
+    train['up_zero'] = train['缴费用户最近一次缴费金额（元）'].map(lambda x: 0 if x != 0 else 1)
+    train['avg_zero'] = train['用户近6个月平均消费值（元）'].map(lambda x: 0 if x != 0 else 1)
+    train['all_zero'] = train['用户账单当月总费用（元）'].map(lambda x: 0 if x != 0 else 1)
+    train['zero_age'] = train['用户年龄'].map(lambda x: 0 if x != 0 else 1)
+    train['top_up_amount_offline'] = 0
+    train['top_up_amount_offline'][
+        (train['缴费用户最近一次缴费金额（元）'] % 10 == 0) & train['缴费用户最近一次缴费金额（元）'] != 0] = 1
+    train['current_fee_stability'] = train['用户账单当月总费用（元）'] / (train['用户近6个月平均消费值（元）'] + 1)
+    train['use_left_rate'] = train['用户账单当月总费用（元）'] / (train['用户当月账户余额（元）'] + 1)
+    std = StandardScaler()
+    minMax = MinMaxScaler()
+    #stacking_y = pd.read_csv('xgb_stacking_y.csv', encoding="utf-8")
+    #train['stacking_y'] = minMax.fit_transform(stacking_y[['xgb_stacking_y']])
+    train['用户年龄归一化'] = std.fit_transform(train[['用户年龄']])
+    train.drop("用户年龄", axis=1, inplace=True)
+    train.drop("是否黑名单客户", axis=1, inplace=True)
     train.drop("用户编码", axis=1, inplace=True)
-    #train.drop("是否大学生客户", axis=1, inplace=True)
-    model_train,model_test,model_label,model_score = train_test_split(train, train["信用分"], train_size=0.8)
+
+    test['用户当月消费是否低于近6个月'] = test.apply(lambda row: 1 if row['用户账单当月总费用（元）'] > row['用户近6个月平均消费值（元）'] else 0, axis=1)
+    test['用户余额能支持月数'] = test.apply(lambda row: row['用户当月账户余额（元）'] / (row['用户近6个月平均消费值（元）'] + 1.0), axis=1)
+    test['用户最近缴费支持月数'] = test.apply(lambda row: (row['缴费用户最近一次缴费金额（元）']) / (row['用户近6个月平均消费值（元）'] + 1.0), axis=1)
+    test['up_zero'] = test['缴费用户最近一次缴费金额（元）'].map(lambda x: 0 if x != 0 else 1)
+    test['avg_zero'] = test['用户近6个月平均消费值（元）'].map(lambda x: 0 if x != 0 else 1)
+    test['all_zero'] = test['用户账单当月总费用（元）'].map(lambda x: 0 if x != 0 else 1)
+    test['zero_age'] = test['用户年龄'].map(lambda x: 0 if x != 0 else 1)
+    test['top_up_amount_offline'] = 0
+    test['top_up_amount_offline'][
+        (test['缴费用户最近一次缴费金额（元）'] % 10 == 0) & test['缴费用户最近一次缴费金额（元）'] != 0] = 1
+    test['current_fee_stability'] = test['用户账单当月总费用（元）'] / (test['用户近6个月平均消费值（元）'] + 1)
+    test['use_left_rate'] = test['用户账单当月总费用（元）'] / (test['用户当月账户余额（元）'] + 1)
+
+
+    test['用户年龄归一化'] = std.fit_transform(test[['用户年龄']])
+    test.drop("用户年龄", axis=1, inplace=True)
+
+    model_train,model_test,model_label,model_score = train_test_split(train, train["信用分"], train_size=0.8, random_state=2019)
     model_train.drop("信用分", axis=1, inplace=True)
     model_test.drop("信用分", axis=1, inplace=True)
-    # model_train.drop("是否大学生客户", axis=1, inplace=True)
-    # model_test.drop("是否大学生客户", axis=1, inplace=True)
     test_data = test.drop("用户编码", axis=1)
     print(model_train.shape)
     print(model_test.shape)
 
     clf = lgb.LGBMRegressor(
-        boosting_type='gbdt', num_leaves=125, reg_alpha=0.0, reg_lambda=1,
-        max_depth=7, n_estimators=3000, objective='regression',
-        subsample=0.9, colsample_bytree=0.7, subsample_freq=1,
-        learning_rate=0.01, min_child_weight=50, random_state=2018, n_jobs=-1, max_bin=200
+        boosting_type='gbdt', num_leaves=63, reg_alpha=0.0, reg_lambda=1,
+        max_depth=5, n_estimators=10000, objective='regression',
+        subsample=0.7, colsample_bytree=0.7, subsample_freq=1,
+        learning_rate=0.01, min_child_weight=50, random_state=2018, n_jobs=-1, max_bin=250
     )
     is_model = True
     if is_model:
-        clf.fit(model_train, model_label)
+        clf.fit(model_train, model_label, eval_set=[(model_train, model_label), (model_test, model_score)], early_stopping_rounds=200, verbose=10)
         print(list(clf.feature_importances_))
         print(model_train.columns)
-        y_predict = clf.predict(model_test)
+        y_predict = clf.predict(model_test, num_iteration=clf.best_iteration_)
         print(metrics_mae(y_predict.tolist(),list(model_score)))
     else:
         label = train['信用分']
         train.drop("信用分", axis=1, inplace=True)
-        train.drop("用户编码", axis=1, inplace=True)
-        clf.fit(train, label)
+        clf.fit(model_train, model_label, eval_set=[(model_train, model_label), (model_test, model_score)],early_stopping_rounds=200, verbose=10)
+        #clf.fit(train, label)
         test['信用分'] = clf.predict(test_data)
 
         data_submit = pd.DataFrame()
         data_submit['id'] = test['用户编码']
         data_submit['score'] = test.apply(lambda row: int(round(row.信用分)), axis=1)
-        data_submit.to_csv("sc_0214_v2.csv", encoding="utf-8", index=False)
+        data_submit.to_csv("submit/sc_lgb_0221_v2.csv", encoding="utf-8", index=False)
 
+#baseline : 0.06304295747122089
+#0.0632519070449974
+#0.06329474463735277
+#0.06368088236230601
+#0.06373201960396924
+#0.06375721253466798
+#0.06376575014028465
+#0.06377103647065575
+#0.063817838362179
+#0.06383820844451821
+#0.06385410613829522
 
-#0.06377062979873989
 
 
